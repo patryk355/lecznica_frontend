719170dad25921f024ffd4f753164257e603de7e
patryk355 <patrykkarasinski11@gmail.com>
2023-12-27 19:25:18 +0100

    Updates

---
+++ assets/Card-_shyN-8G.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-6cAbl0Yg.js";const c=({children:r,bgColor:s,className:o="",onClick:a})=>t.jsx("div",{className:`card ${o}`,style:{backgroundColor:s},onClick:a,children:r});export{c as C};
---
+++ assets/ProtectedLayout-_wl_Ny6d.js
@@ -0,0 +1,1 @@
+import{j as t,N as s,O as r}from"./index-6cAbl0Yg.js";const e=()=>t.jsxs(t.Fragment,{children:[t.jsx(s,{}),t.jsx("main",{children:t.jsx(r,{})})]});export{e as default};
