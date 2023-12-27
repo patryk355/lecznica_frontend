f742e5880ad0b04ffa4cf17b2b04574c71ccd611
patryk355 <patrykkarasinski11@gmail.com>
2023-12-27 21:25:18 +0100

    Changed BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import App from './App.jsx';
 import {AppContextProvider} from './context/appContext.jsx';
@@ -11,10 +11,10 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <AppContextProvider>
-            <BrowserRouter>
+            <HashRouter basename={'/'}>
                 <App/>
                 <ToastContainer autoClose={2500}/>
-            </BrowserRouter>
+            </HashRouter>
         </AppContextProvider>
     </React.StrictMode>
 );
